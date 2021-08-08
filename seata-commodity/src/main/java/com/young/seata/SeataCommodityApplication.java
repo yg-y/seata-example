3191540f521f85be3c9e2ef6ac4830ed788b2aad
young <y627756022@163.com>
2021-08-09 00:12:10 +0800

    update

--- seata-commodity/src/main/java/com/young/seata/SeataCommodityApplication.java
+++ seata-commodity/src/main/java/com/young/seata/SeataCommodityApplication.java
@@ -1,11 +1,22 @@
 package com.young.seata;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.ConfigService;
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Properties;
+
+@RequestMapping("/app")
+@RestController
 @EnableTransactionManagement
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -15,4 +26,5 @@ public class SeataCommodityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SeataCommodityApplication.class, args);
     }
+
 }
--- seata-commodity/src/test/java/com/young/seata/SeataCommodityApplicationTests.java
+++ seata-commodity/src/test/java/com/young/seata/SeataCommodityApplicationTests.java
@@ -1,18 +1,30 @@
-//package com.young.seata;
-//
-//import org.junit.Test;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class SeataCommodityApplicationTests {
-//
-//    @Value("$server.port")
-//    String value;
-//
-//    @Test
-//    public void contextLoads() {
-//        System.err.println(value);
-//    }
-//
-//}
+package com.young.seata;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeataCommodityApplicationTests {
+
+    @Value("$server.port")
+    String value;
+
+    @Test
+    public void contextLoads() {
+        System.err.println(value);
+    }
+
+    @Autowired
+    @NacosInjected
+    private ConfigService configService;
+
+    @Test
+    public void testPublishUser() throws NacosException {
+        System.err.println(configService.getConfig("service.vgroupMapping.my_test_tx_group", "DEFAULT_GROUP", 3000l));
+    }
+}
